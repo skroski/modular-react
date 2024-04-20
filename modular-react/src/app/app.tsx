b6a000336460701d69961796971101d11e378bd6
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2024-04-20 16:30:47 -0300

    fix: orders list name

--- modular-react/src/app/app.tsx
+++ modular-react/src/app/app.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Route, Routes } from 'react-router-dom';
 import {ProductList} from  '@modular-react/modules/products';
-import { OrderList} from '@modular-react/modules/orders';
+import { OrdersList} from '@modular-react/modules/orders';
 function Home() {
   return <h1>Home</h1>
 }
@@ -10,7 +10,7 @@ export function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<ProductList />} />
-      <Route path='/orders' element={<OrderList />} />
+      <Route path='/orders' element={<OrdersList />} />
     </Routes>
   );
 }
